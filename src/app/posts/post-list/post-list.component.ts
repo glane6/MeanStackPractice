31797c1925ed7a44d05e8fd0f2b5b0c6c50af003
Greg Lane <g.j.lane90@gmail.com>
2019-05-11 18:40:51 -0400

    Added header and post-list componenets

---
+++ src/app/posts/post-list/post-list.component.ts
@@ -0,0 +1,14 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: 'app-post-list',
+  templateUrl: './post-list.component.html',
+  styleUrls: ['./post-list.component.css']
+})
+export class PostListComponent{
+  posts = [{
+    title: 'First Post', Content: 'This is the first post content'},
+    {title: 'Second Post', Content: 'This is the second post content'},
+    {title: 'Third Post', Content: 'This is the third post content'}
+  ];
+}
